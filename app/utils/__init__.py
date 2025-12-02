e02d589c6e95aa5dc5ef0e2819710062235a4937
diogogosch <59872591+diogogosch@users.noreply.github.com>
2025-12-02 18:43:31 -0300

    Update utils __init__.py with proper exports for all utilities

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,1 +1,13 @@
 
+"""Utility module initialization."""
+from app.utils.logger import setup_logger
+from app.utils.i18n import i18n
+from app.utils.validators import validators
+from app.utils.helpers import helpers
+
+__all__ = [
+    "setup_logger",
+    "i18n",
+    "validators",
+    "helpers",
+]
