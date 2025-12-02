bc46d9e6af645f8fc6764b17bcc832f48cb984e2
diogogosch <59872591+diogogosch@users.noreply.github.com>
2025-12-02 17:38:37 -0300

    Update services/__init__.py with service imports

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,7 @@
 
+"""Services module initialization"""
+from .ai_service import ai_service
+from .ocr_service import ocr_service
+from .notification_service import notification_service
+
+__all__ = ["ai_service", "ocr_service", "notification_service"]
